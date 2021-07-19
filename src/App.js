1954c838f767a8ddbc40d29c1909c1c97d063436
Francesco <fdenaro96@gmail.com>
2021-07-19 11:49:59 +0200

    First commit and set up of Firebase connection

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
+import firebase from 'firebase';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore'
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDPGphDBs38UJtP5Vx6rlk3R3EOoiu8ZFs",
+  authDomain: "superchat-56f76.firebaseapp.com",
+  projectId: "superchat-56f76",
+  storageBucket: "superchat-56f76.appspot.com",
+  messagingSenderId: "563508821538",
+  appId: "1:563508821538:web:c830c625090105f2f4147b",
+  measurementId: "G-57X2YJ57VQ"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
